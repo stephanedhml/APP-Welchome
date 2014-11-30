542e7b44cc03485cf18dba2908da856ad7e5d0cb
Geekelektro <beaudru.manuel@gmail.com>
2014-11-30 14:37:06 +0100

    Réparation du responsive HS suite à chgt apparence barre menu

--- menu_responsivet/config.php
+++ menu_responsivet/config.php
@@ -1,7 +1,7 @@
 <?php
 try
 	{
-		$bdd = new PDO ('mysql:host=localhost;dbname=welchome', 'root', 'root', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+		$bdd = new PDO ('mysql:host=localhost;dbname=welchome', 'root', '', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
 	}
 	catch(Exception $e)
 	{
