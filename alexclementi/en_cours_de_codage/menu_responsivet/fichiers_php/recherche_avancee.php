1130ad9e218c6648f07b2ad9d2ef473be61ebd78
Geekelektro <beaudru.manuel@gmail.com>
2014-12-01 17:00:37 +0100

    Modification localisation des fichiers + ajout d'un css pour la page recherche avancée

--- alexclementi/en_cours_de_codage/menu_responsivet/fichiers_php/recherche_avancee.php
+++ alexclementi/en_cours_de_codage/menu_responsivet/fichiers_php/recherche_avancee.php
@@ -1,11 +1,23 @@
+<?php
+include("config.php");
+session_start();
+?>
+
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr" lang="fr">
     <head>
-        <meta charset="utf-8" />    
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-         <title>Recherche avancée</title>		
-         </head>
+        <link rel="stylesheet" href="../style.css" />
+        <?php include("../menu_responsive/javascript/menu_responsive.js"); ?>
+        <title>Recherche avancée</title>
+        </head>
+
     <body>
+    <div class="header">
+        <?php include("menus.php"); ?>
+    </div>
+
+    <div class="formulaire_r_avancee">
 	<form method="post" action="cible_recherche.php">
 <pre>       
 	   Quel type de logement souhaitez-vous ?<br/>
@@ -67,5 +79,6 @@ Cochez les attributs qui vous intéressent parmi les suivants<br/>
 		   	       <input type="submit" value="Valider" /><br/>
 </pre>			   
 </form>
+        </div>
     </body>
 </html>
