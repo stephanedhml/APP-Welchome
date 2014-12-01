2bef58130caf41fb19d9bd355744e8354b78d7e5
Geekelektro <beaudru.manuel@gmail.com>
2014-12-01 17:53:48 +0100

    Modification de l'apparence du formulaire de recherche ainsi que de la couleur des boutons des pages connexion/inscription/recherche_avancee

--- alexclementi/en_cours_de_codage/menu_responsivet/fichiers_php/menus.php
+++ alexclementi/en_cours_de_codage/menu_responsivet/fichiers_php/menus.php
@@ -6,7 +6,7 @@
 		<ul class="clearfix">
 			<li><a href="accueilmanu.php">Accueil</a></li>
 			<li><a href="#">Offres</a></li>
-			<li class="recherche_avancee"><a href="#">Recherche avancée</a></li>
+			<li class="recherche_avancee"><a href="recherche_avancee.php">Recherche avancée</a></li>
 			<li><a href="#">Forum</a></li>
             <?php
 				include("connexion_inscription_deconnexion_menu.php"); 
--- alexclementi/en_cours_de_codage/menu_responsivet/fichiers_php/recherche_avancee.php
+++ alexclementi/en_cours_de_codage/menu_responsivet/fichiers_php/recherche_avancee.php
@@ -19,8 +19,7 @@ session_start();
 
     <div class="formulaire_r_avancee">
 	<form method="post" action="cible_recherche.php">
-<pre>       
-	   <p>Quel type de logement souhaitez-vous ?</p><br/>
+	   <p>Quel type de logement souhaitez-vous ?<br/>
 <select name="type_logement">
     <option value="choix1">Studio</option>
     <option value="choix2">Appartement</option>
@@ -28,14 +27,14 @@ session_start();
     <option value="choix4">Pavillon</option>
     <option value="choix5">Bungalow/Gîte</option>
     <option value="choix6">Bateau/Péniche</option>
-    <option value="choix7">Camping car</option>
+    <option value="choix7">Camping car</option></p>
 
 </select><br/>
 
-          Veuillez entrez les dates qui vous conviennent:
-          de<input type="date" name="d1" value="JJ/MM/AAAA"> à<input type="date" name="d2" value="JJ/MM/AAAA">
+        <p>Veuillez entrez les dates qui vous conviennent:<br/>
+          de <input type="date" name="d1" value="JJ/MM/AAAA"> à <input type="date" name="d2" value="JJ/MM/AAAA"></p>
 
-Sélectionnez le nombre de personnes qui vous accompagneront<br/>
+<p>Sélectionnez le nombre de personnes qui vous accompagneront<br/>
 <select name="nombre">
     <option value="choix1">1</option>
     <option value="choix2">2</option>
@@ -44,20 +43,20 @@ Sélectionnez le nombre de personnes qui vous accompagneront<br/>
     <option value="choix4">5</option>
     <option value="choix4">6</option>
     <option value="choix4">7 ou plus</option>
-</select><br/>
+</select><br/></p>
 
-Entrer la surface minimale du logement que vous souhaitez.
-<input type="number" name="surface_min" value="Surface minimale">
+<p>Entrer la surface minimale du logement que vous souhaitez.<br/>
+<input type="number" name="surface_min" value="Surface minimale"></p>
     
-Avez-vous des préférences parmi les propositions suivantes ?
+<p>Avez-vous des préférences parmi les propositions suivantes ?<br/>
 <input type="checkbox" name="lieu1" id="case" /> <label for="case">Banlieue</label><br/>
 <input type="checkbox" name="lieu2" id="case" /> <label for="case">Campagne</label><br/>
 <input type="checkbox" name="lieu3" id="case" /> <label for="case">Montagne</label><br/>
-<input type="checkbox" name="lieu4" id="case" /> <label for="case">Ville</label><br/>
+<input type="checkbox" name="lieu4" id="case" /> <label for="case">Ville</label><br/></p>
 
 
 	   
-Cochez les attributs qui vous intéressent parmi les suivants<br/>
+<p>Cochez les attributs qui vous intéressent parmi les suivants<br/>
 <input type="checkbox" name="case1" id="case" /> <label for="case">Animaux acceptés</label><br/>
 <input type="checkbox" name="case2" id="case" /> <label for="case">Climatisation</label><br/>
 <input type="checkbox" name="case3" id="case" /> <label for="case">Chauffage</label><br/>
@@ -69,15 +68,10 @@ Cochez les attributs qui vous intéressent parmi les suivants<br/>
 <input type="checkbox" name="case9" id="case" /> <label for="case">Piscine</label><br/>
 <input type="checkbox" name="case10" id="case" /> <label for="case">Jardin</label><br/>
 <input type="checkbox" name="case11" id="case" /> <label for="case">Balcon</label><br/>
-<input type="checkbox" name="case12" id="case" /> <label for="case">Internet sans fil</label><br/>
-	 
-		   
-		   Avez-vous quelque chose a ajouter ?<br/>
-		   <textarea name="message" rows="8" cols="45">
-          Votre message ici.
-           </textarea>
-		   	       <input type="submit" value="Valider" /><br/>
-</pre>			   
+<input type="checkbox" name="case12" id="case" /> <label for="case">Internet sans fil</label><br/><br/></p>
+
+    <p><input type="submit" value="Valider" id="btn_valider"/></p><br/>
+</pre>
 </form>
         </div>
     </body>
