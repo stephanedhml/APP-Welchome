6edb1f05f1a745171feb5eca91be3ee236f9428c
Geekelektro <beaudru.manuel@gmail.com>
2014-12-05 10:42:31 +0100

    Ajout des images carrousel

--- fichiers_php/accueilmanu.php
+++ fichiers_php/accueilmanu.php
@@ -27,7 +27,7 @@
 			<div id="partie2">
 				<div class="h2_p2">
 					<p>
-						Proposez vos dates, échangez vos logements
+						Trouvez un logement qui vous correspond
 					</p>
 				</div>
 				<div class="schema1">
@@ -41,7 +41,7 @@
                             <div class="plan p2"><img src="../wall/wall6.jpg" alt="" class="w2"></div>
                             <div class="plan p3"><img src="../wall/wall5.jpg" alt="" class="w3"></div>
                             <div class="plan p4"><img src="../wall/wall4.jpg" alt="" class="w4"></div>
-                            <div class="plan p5"><img src="../wall/wall3.jpg" alt="" class="w5"></div>
+                            <div class="plan p5"><a href="http://www.google.fr"><img src="../wall/wall3.jpg" alt="" class="w5"></a></div>
                             <div class="plan p6"><img src="../wall/wall2.jpeg" alt="" class="w6"></div>
                             <div class="plan p7"><img src="../wall/wall1.jpg" alt="" class="w7"></div>
                             <div class="plan p8"><img src="../wall/wall4.jpg" alt="" class="w8"></div>
@@ -49,6 +49,16 @@
 
                     </div>
 				</div>
+
+                <div class="h2_p2">
+                    <p>
+                        Nouvelles technologies pour vous !
+                    </p>
+                </div>
+
+                <div class="schema2">
+                    <img src="../images_diverses/htmlcssjs_logo.png" class="html5_logo">
+                </div>
 			</div>
 			
 			<!--<div id="partie3">
