ce89ffc6280c78021abb037cee2f5a811d1cb9fa
Geekelektro <beaudru.manuel@gmail.com>
2014-12-12 21:46:45 +0100

    Réparation du formulaire d'inscription

--- fichiers_php/sign_up.php
+++ fichiers_php/sign_up.php
@@ -21,117 +21,117 @@
 		</div>
         <div class="superglobal">
         <div class="global">
-		<?php
-			//Vérification du bon envoi du formulaire
-			if(isset($_POST['username'], $_POST['password'], $_POST['passverif'], $_POST['email'], $_POST['avatar'], $_POST['lastname'], $_POST['firstname'], $_POST['genre'], $_POST['tel']) and $_POST['username']!='')
-			{
-				//On vérifie que les deux mots de passe coïncident
-				if($_POST['password']==$_POST['passverif'])
-				{
-					//On vérifie si le mdp contient bien 6 caract. mini
-					if(strlen($_POST['password'])>=6)
-					{
-						//On vérifie si l'email est valide
-						if(preg_match('#^(([a-z0-9!\#$%&\\\'*+/=?^_`{|}~-]+\.?)*[a-z0-9!\#$%&\\\'*+/=?^_`{|}~-]+)@(([a-z0-9-_]+\.?)*[a-z0-9-_]+)\.[a-z]{2,}$#i',$_POST['email']))
-						{
+            <?php
+            //Vérification du bon envoi du formulaire
+            if(isset($_POST['username'], $_POST['password'], $_POST['passverif'], $_POST['email'], $_POST['avatar']) and $_POST['username']!='')
+            {
+                //On vérifie que les deux mots de passe coïncident
+                if($_POST['password']==$_POST['passverif'])
+                {
+                    //On vérifie si le mdp contient bien 6 caract. mini
+                    if(strlen($_POST['password'])>=6)
+                    {
+                        //On vérifie si l'email est valide
+                        if(preg_match('#^(([a-z0-9!\#$%&\\\'*+/=?^_`{|}~-]+\.?)*[a-z0-9!\#$%&\\\'*+/=?^_`{|}~-]+)@(([a-z0-9-_]+\.?)*[a-z0-9-_]+)\.[a-z]{2,}$#i',$_POST['email']))
+                        {
+                            // Hachage du mot de passe
+                            $pass_hache = sha1($_POST["password"]);
 
 
-							//On vérifie si le pseudo n'existe pas déjà
-							$res=recuperer_username();
-
-							if(!$res)
-							{
-								//On insère les données saisies par l'utilisateur dans la BD
-								$req =add_user_datas();
-
-								//On enregistre les infos dans la base de donnée
-								if($bdd->lastInsertId())
-								{
-									//Si ça a fonctionné, on affiche pas le formulaire
-									$form=false;
-		?>
-									<div class='message'>Vous avez bien été inscrit. Vous pouvez désormais vous connecter !
-									</br><a href="connexion.php">Se connecter</a>
-									</div>
-		<?php
-								}
-								else
-								{
-									//Sinon, il y a une erreur
-									$form=true;
-									$message = "Une erreur est survenue lors de l'inscription.";
-								}
-							}
-							else
-							{
-								//Si il existe déjà, on en informe l'utilisateur
-								$form=true;
-								$message="Désolé, ce pseudo est déjà pris !";
-							}
-						}
-						else
-						{
-							//L'email n'est pas valide, message d'erreur
-							$form=true;
-							$message="L'email rentrée n'est pas valide.";
-						}
-					}
-					else
-					{
-						//Le mdp ne fait pas 6 caractères
-						$form=true;
-						$message="Désolé, votre mot de passe doit faire au moins 6 caractères.";
-					}
-				}
-				else
-				{
-					//Les mdp ne coïncident pas
-					$form=true;
-					$message="Les mots de passe rentrés ne sont pas identiques.";
-				}
-			}
-			else
-			{
-				//Le formulaire n'a pas été bien envoyé
-				$form=true;
-			}
-
-
-
-		if($form)
-		{
-			if(isset($message))
-			{
-				echo '<div class="message">'.$message.'</div>';
-			}
-			// On affiche le formulaire
-		?>
+                            //On vérifie si le pseudo n'existe pas déjà
+                            $req = $bdd->prepare('SELECT id FROM users WHERE username=?');
+                            $req->execute(array($_POST['username']));
+                            $res = $req->fetch();
+                            if(!$res)
+                            {
+                                //On insère les données saisies par l'utilisateur dans la BDD
+                                $req = $bdd->prepare('INSERT INTO users(username,password,email,avatar) VALUES(:username, :password, :email, :avatar)');
+                                $req->execute(array(
+                                    'username' => $_POST["username"],
+                                    'password' => $pass_hache,
+                                    'email' => $_POST["email"],
+                                    'avatar' => $_POST["avatar"],
+                                ));
+                                //On enregistre les infos dans la base de donnée
+                                if($bdd->lastInsertId())
+                                {
+                                    //Si ça a fonctionné, on affiche pas le formulaire
+                                    $form=false;
+                                    ?>
+                                    <div class='message'>Vous avez bien été inscrit. Vous pouvez désormais vous connecter !
+                                        </br><a href="connexion.php">Se connecter</a></div>
+                                <?php
+                                }
+                                else
+                                {
+                                    //Sinon, il y a une erreur
+                                    $form=true;
+                                    $message = "Une erreur est survenue lors de l'inscription.";
+                                }
+                            }
+                            else
+                            {
+                                //Si il existe déjà, on en informe l'utilisateur
+                                $form=true;
+                                $message="Désolé, ce pseudo est déjà pris !";
+                            }
+                        }
+                        else
+                        {
+                            //L'email n'est pas valide, message d'erreur
+                            $form=true;
+                            $message="L'email rentrée n'est pas valide.";
+                        }
+                    }
+                    else
+                    {
+                        //Le mdp ne fait pas 6 caractères
+                        $form=true;
+                        $message="Désolé, votre mot de passe doit faire au moins 6 caractères.";
+                    }
+                }
+                else
+                {
+                    //Les mdp ne coïncident pas
+                    $form=true;
+                    $message="Les mots de passe rentrés ne sont pas identiques.";
+                }
+            }
+            else
+            {
+                //Le formulaire n'a pas été bien envoyé
+                $form=true;
+            }
+            if($form)
+            {
+            if(isset($message))
+            {
+                echo '<div class="message">'.$message.'</div>';
+            }
+            // On affiche le formulaire
+            ?>
 			<div class="cadre">
             <div class="contentg">
 
             </div>
             <div class="contentd1">
                 <div class="signup_form1">
-                <form>
+                <form action="sign_up.php" method="post">
                     <label for="username">Nom d'utilisateur</label><br/><input type="text" name="username" value="<?php if(isset($_POST["username"])){echo htmlentities($_POST["username"], ENT_QUOTES,"UTF-8");} ?>" /></br>
                     <br/><label for="password">Mot de passe<span class="small"> (6 caractères minimum)</span></label><br/><input type="password" name="password" /><br />
                     <br/><label for="passverif">Mot de passe<span class="small"> (vérification)</span></label><br/><input type="password" name="passverif" /><br />
                     <br/><label for="email">Email</label><br/><input type="text" name="email" value="<?php if(isset($_POST['email'])){echo htmlentities($_POST['email'], ENT_QUOTES, 'UTF-8');} ?>" /><br />
                     <br/><label for="avatar">Image perso<span class="small"> (facultatif)</span></label><br/><input type="text" name="avatar" value="<?php if(isset($_POST['avatar'])){echo htmlentities($_POST['avatar'], ENT_QUOTES, 'UTF-8');} ?>" /><br />
                     <br/><input type="submit" value="Envoyer" id="btn_envoyer" />
-
+                </form>
                 </div>
 
             </div>
             <div class="contentd2">
                 <div class="signup_form2">
-                    <br/><label for="lastname">Nom :</label><br/><input type="text" name="lastname" /><br />
-                    <br/><label for="firstname">Prénom :</label><br/><input type="text" name="firstname" /><br />
-                    <br/><label for="genre">Genre :</label><br/><span>M</span><input type="radio" name="genre" /><span>F</span><input type="radio" name="genre" /><br />
-                    <br/><label for="tel">Tel :</label><br/><input type="tel" name="tel"/><br/>
 
                 </div>
-                </form>
+
             </div>
 		<?php
 		}
