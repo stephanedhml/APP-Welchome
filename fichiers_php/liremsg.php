fa2a7552d5c75f50c03cd24dab45f8b9a2dba056
Geekelektro <beaudru.manuel@gmail.com>
2014-12-15 22:27:39 +0100

    Affiche les messages reçus dans des tableaux

--- fichiers_php/liremsg.php
+++ fichiers_php/liremsg.php
@@ -11,3 +11,34 @@ if (!isset($_SESSION["userid"]))
     header ("Location: accueilmanu.php");
     exit();
 }
+
+$req = $bdd -> prepare("SELECT id_expediteur, titre, message FROM messages WHERE id_destinataire=?");
+$req -> execute(array($_SESSION["userid"]));
+$nb = $req -> rowCount();
+
+
+
+if ($nb == 0) {
+    echo 'Aucun message';
+}
+else {
+    for ($i=0 ; $i < $nb ; $i++) {
+        $msg_recu = $req -> fetch();
+        ?>
+        <table border="1" width="400" style="text-align: center;">
+            <tr>
+                <td>id_expediteur</td>
+                <td>Objet</td>
+                <td>Message</td>
+
+            </tr>
+            <tr>
+                <td><?php echo $msg_recu[0]; ?></td>
+                <td><?php echo $msg_recu[1]; ?></td>
+                <td><?php echo $msg_recu[2]; ?></td><br/>
+            </tr>
+        </table>
+
+    <?php
+    }
+}
