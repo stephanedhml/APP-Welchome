6a4ae350978a94c4596175ede51e2916fdb59c70
Geekelektro <beaudru.manuel@gmail.com>
2014-12-15 22:38:39 +0100

    Association de l'id_user au pseudo de l'expéditeur dans les résultats de l'array

--- fichiers_php/liremsg.php
+++ fichiers_php/liremsg.php
@@ -17,23 +17,25 @@ $req -> execute(array($_SESSION["userid"]));
 $nb = $req -> rowCount();
 
 
-
 if ($nb == 0) {
     echo 'Aucun message';
 }
 else {
     for ($i=0 ; $i < $nb ; $i++) {
         $msg_recu = $req -> fetch();
+        $quser = $bdd -> prepare("SELECT username FROM users WHERE id=?");
+        $quser -> execute(array($msg_recu[0]));
+        $un = $quser -> fetch();
         ?>
         <table border="1" width="400" style="text-align: center;">
             <tr>
-                <td>id_expediteur</td>
+                <td>Nom exp&#233;diteur</td>
                 <td>Objet</td>
                 <td>Message</td>
 
             </tr>
             <tr>
-                <td><?php echo $msg_recu[0]; ?></td>
+                <td><?php echo $un[0]; ?></td>
                 <td><?php echo $msg_recu[1]; ?></td>
                 <td><?php echo $msg_recu[2]; ?></td><br/>
             </tr>
