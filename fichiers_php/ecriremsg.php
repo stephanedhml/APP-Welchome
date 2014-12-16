27bf577576de2de989a289c75a6c4b31d125bc91
Geekelektro <beaudru.manuel@gmail.com>
2014-12-16 04:38:53 +0100

    Grosse MAJ visuelle du tableau des messages + ajout d'une règle UTF8 pour les requêtes SQL

--- fichiers_php/ecriremsg.php
+++ fichiers_php/ecriremsg.php
@@ -1,73 +1,95 @@
 <?php
 include("config.php");
 include("modeles.php");
+include("../menu_responsive/javascript/menu_responsive.js");
 
 session_start();
 ?>
 
-<?php
-if (!isset($_SESSION["userid"]))
-{
-    header ("Location: accueilmanu.php");
-    exit();
-}
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <link rel="shortcut icon" href="../images_diverses/icon.png" type="image/x-icon"/>
+        <link rel="icon" href="../images_diverses/icon.png" type="image/x-icon"/>
+        <link rel="stylesheet" href="../style.css" />
+    </head>
+<body>
 
-if (isset($_POST["destinataire"],$_POST["titre"], $_POST["message"]))
-{
-    $titre = $_POST["titre"];
-    $message = $_POST["message"];
-    $userid = $_SESSION["userid"];
-    //On fait correspondre le pseudo du destinataire avec son id
-    $destinataire = $_POST["destinataire"];
-    $req = $bdd -> prepare("SELECT id FROM users WHERE username = ? ");
-    $req -> execute(array($destinataire));
-    $dn = $req -> fetch();
+        <header>
+            <?php include("menus.php"); ?>
+        </header>
+    <div class="superglobal">
+        <div class="global">
 
-    $res = $bdd -> prepare("INSERT INTO messages(id_destinataire,id_expediteur,titre,message) VALUES(:destinataire,:expediteur,:titre, :message)");
-    $res -> execute(array(
-        "destinataire" => $dn[0],
-        "expediteur" => $userid,
-        "titre" => $titre,
-        "message" => $message,
-    ));
-
-    if($bdd -> lastInsertId())
+            <div id="bloc_page_msg">
+    <?php
+    if (!isset($_SESSION["userid"]))
     {
-        ?>
-        <div>Votre message a bien &#233;t&#233; envoy&#233; !</div>
-        <a href="accueilmanu.php">Retourner à l'accueil</a>
-        <?php
-    }
-    else
-    {
-        ?>
-        <div>Il y a eu un problème lors de l'envoi de votre message, veuillez r&#233;essayer.</div> <br/>
-        <a href="accueilmanu.php">Retourner à l'accueil</a>
-        <?php
+        header ("Location: accueilmanu.php");
+        exit();
     }
 
-}
+    if (isset($_POST["destinataire"],$_POST["titre"], $_POST["message"]))
+    {
+        $titre = $_POST["titre"];
+        $message = $_POST["message"];
+        $userid = $_SESSION["userid"];
+        //On fait correspondre le pseudo du destinataire avec son id
+        $destinataire = $_POST["destinataire"];
+        $req = $bdd -> prepare("SELECT id FROM users WHERE username = ? ");
+        $req -> execute(array($destinataire));
+        $dn = $req -> fetch();
 
-$req = $bdd -> prepare("SELECT username FROM users");
-$req -> execute(array());
-$nb = $req -> rowCount();
+        $res = $bdd -> prepare("INSERT INTO messages(id_destinataire,id_expediteur,titre,message) VALUES(:destinataire,:expediteur,:titre, :message)");
+        $res -> execute(array(
+            "destinataire" => $dn[0],
+            "expediteur" => $userid,
+            "titre" => $titre,
+            "message" => $message,
+        ));
 
-?>
-
-<form action="ecriremsg.php" method="post" xmlns="http://www.w3.org/1999/html">
-    <select name="destinataire">
-        <?php
-        for ($i=0 ; $i < $nb ; $i++) {
-            $username = $req -> fetch();
+        if($bdd -> lastInsertId())
+        {
             ?>
-            <option value='<?php echo $username[0] ?>' ><?php echo $username[0]?></option>
+            <div>Votre message a bien &#233;t&#233; envoy&#233; !</div>
+            <a href="accueilmanu.php">Retourner à l'accueil</a>
             <?php
         }
-        ?>
-    </select> <br />
-    <label for="titre">Titre du message</label>
-    <input type="text" name="titre"> <br/>
-    <label for="message">Message</label>
-    <input type="text" name="message">
-    <input type="submit" value="Envoyer">
-</form>
+        else
+        {
+            ?>
+            <div>Il y a eu un problème lors de l'envoi de votre message, veuillez r&#233;essayer.</div> <br/>
+            <a href="accueilmanu.php">Retourner à l'accueil</a>
+            <?php
+        }
+
+    }
+
+    $req = $bdd -> prepare("SELECT username FROM users");
+    $req -> execute(array());
+    $nb = $req -> rowCount();
+
+    ?>
+
+    <form action="ecriremsg.php" method="post" xmlns="http://www.w3.org/1999/html">
+        <select name="destinataire">
+            <?php
+            for ($i=0 ; $i < $nb ; $i++) {
+                $username = $req -> fetch();
+                ?>
+                <option value='<?php echo $username[0] ?>' ><?php echo $username[0]?></option>
+                <?php
+            }
+            ?>
+        </select> <br />
+        <label for="titre">Titre du message</label>
+        <input type="text" name="titre"> <br/>
+        <label for="message">Message</label>
+        <input type="text" name="message">
+        <input type="submit" value="Envoyer">
+    </form>
+                </div>
+            </div>
+        </div>
+</body>
+</html>
--- fichiers_php/liremsg.php
+++ fichiers_php/liremsg.php
@@ -7,7 +7,7 @@ session_start();
 ?>
 <html>
     <head>
-        <meta charset="utf-8" />
+        <meta charset="UTF-8">
         <link rel="shortcut icon" href="../images_diverses/icon.png" type="image/x-icon"/>
         <link rel="icon" href="../images_diverses/icon.png" type="image/x-icon"/>
         <link rel="stylesheet" href="../style.css" />
@@ -37,27 +37,26 @@ session_start();
                     echo '<div class="no_msg"><p>Aucun message <br/><br/> <a href="ecriremsg.php" id="btn_connexion">Envoyer un message</a></p></div>';
                 }
                 else {
-                    echo 'Vous avez des messages : <br/>';
+                    echo '<div class="new_msg"> Vous avez des messages : <br/>';
                     for ($i=0 ; $i < $nb ; $i++) {
                         $msg_recu = $req -> fetch();
                         $quser = $bdd -> prepare("SELECT username FROM users WHERE id=?");
                         $quser -> execute(array($msg_recu[0]));
                         $un = $quser -> fetch();
                         ?>
-                        <table border="1" width="400" style="text-align: center;">
+                        <table class="tableau_new_messages" ">
                             <tr>
-                                <td>Nom exp&#233;diteur</td>
-                                <td>Objet</td>
-                                <td>Message</td>
+                                <th>Nom exp&#233;diteur</td>
+                                <th>Objet</td>
+                                <th>Message</td>
 
                             </tr>
                             <tr>
-                                <td><?php echo $un[0]; ?></td>
-                                <td><?php echo $msg_recu[1]; ?></td>
-                                <td><?php echo $msg_recu[2]; ?></td><br/>
+                                <td class="column_msg_1"><?php echo $un[0]; ?></td>
+                                <td class="column_msg_2"><?php echo $msg_recu[1]; ?></td>
+                                <td class="column_msg_3"><?php echo $msg_recu[2]; ?></td><br/>
                             </tr>
                         </table>
-
                     <?php
                     }
                 }
