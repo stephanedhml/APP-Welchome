29ac62acb00988fa7bb62353fd709242e14e8a38
Geekelektro <beaudru.manuel@gmail.com>
2014-12-17 05:31:09 +0100

    Prise en charge de la date de proposition de l'échange

--- fichiers_php/echange.php
+++ fichiers_php/echange.php
@@ -1,7 +1,8 @@
 <?php
-$req = $bdd -> prepare('INSERT INTO echange(id_demandeur, id_proprietaire, id_logement) VALUES(:demandeur, :proprietaire, :logement)');
+$req = $bdd -> prepare('INSERT INTO echange(id_demandeur, id_proprietaire, id_logement, date_update) VALUES(:demandeur, :proprietaire, :logement, :date_update)');
 $req -> execute(array(
     "demandeur" => $_SESSION["userid"],
     "proprietaire" => $donnees1['id'],
     "logement" => $donnees['id'],
+    "date_update" => date("Y-m-d H:i:s"),
 ));
