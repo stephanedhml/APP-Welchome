09024c4b5561a0debd716aef1e05ba430cf39120
Geekelektro <beaudru.manuel@gmail.com>
2014-12-17 06:35:28 +0100

    gros progrès dans le module d'echange (envoi d'un message personnalisé et ajout dans la BDD de la demande)

--- fichiers_php/annonce.php
+++ fichiers_php/annonce.php
@@ -129,7 +129,7 @@
                   Consulter profil
                 </button>
                 <button type="button" class="bouton">
-                    <a href="echange_msg.php">Proposer un échange</a>
+                    <a href="echg_msg.php?demandeur=<?php echo $_SESSION["userid"];?>&proprietaire=<?php echo $donnees1["id"]; ?>&logement=<?php echo $donnees["id"] ;?>">Proposer un échange</a>
                 </button>
                 </section>
 
---
+++ fichiers_php/echg_msg.php
@@ -0,0 +1,101 @@
+<?php
+include("config.php");
+include("modeles.php");
+include("../menu_responsive/javascript/menu_responsive.js");
+
+session_start();
+?>
+
+<html>
+<head>
+    <meta charset="utf-8" />
+    <link rel="shortcut icon" href="../images_diverses/icon.png" type="image/x-icon"/>
+    <link rel="icon" href="../images_diverses/icon.png" type="image/x-icon"/>
+    <link rel="stylesheet" href="../style.css" />
+</head>
+<body>
+
+<header>
+    <?php include("menus.php"); ?>
+</header>
+<div class="superglobal">
+    <div class="global">
+
+        <div id="bloc_page_msg">
+            <?php
+            if (!isset($_SESSION["userid"]))
+            {
+                header ("Location: accueilmanu.php");
+                exit();
+            }
+
+            if (isset($_POST["message"]))
+            {
+                $message = $_POST["message"];
+                $userid = $_SESSION["userid"];
+
+
+            $req = $bdd -> prepare('INSERT INTO echange(id_demandeur, id_proprietaire, id_logement, date_update) VALUES(:demandeur, :proprietaire, :logement, :date_update)');
+            $req -> execute(array(
+                "demandeur" => $_GET['demandeur'],
+                "proprietaire" => $_GET['proprietaire'],
+                "logement" => $_GET['logement'],
+                "date_update" => date("Y-m-d H:i:s"),
+            ));
+
+            $res = $bdd -> prepare("INSERT INTO messages(id_destinataire,id_expediteur,date_update,titre,message) VALUES(:destinataire,:expediteur,:dates,:titre, :message)");
+            $res -> execute(array(
+                "destinataire" => $_GET['proprietaire'],
+                "expediteur" => $_SESSION["userid"],
+                "dates" => $date = date("Y-m-d H:i:s"),
+                "titre" => "Proposition d'échange",
+                "message" => $message,
+            ));
+
+                if($derid = $bdd -> lastInsertId())
+                {
+                    $nv = $bdd -> prepare("UPDATE messages SET lu_nonlu = 1 WHERE id=?");
+                    $nv -> execute(array($derid));
+                    ?>
+                    <div class="no_msg"><h7>Votre demande a bien été transmise !</h7><br/><br/>
+                        <a href="accueilmanu.php">Retourner à l'accueil</a></div>
+                <?php
+                }
+                else
+                {
+                    ?>
+                    <div>Il y a eu un problème lors de l'envoi de votre message, veuillez r&#233;essayer.</div> <br/>
+                    <a href="accueilmanu.php">Retourner à l'accueil</a>
+                <?php
+                }
+
+            }
+
+            $req = $bdd -> prepare("SELECT username FROM users");
+            $req -> execute(array());
+            $nb = $req -> rowCount();
+
+            ?>
+
+            <?php
+                $demandeur = $_GET["demandeur"];
+                $proprietaire = $_GET["proprietaire"];
+                $logement = $_GET["logement"];
+            ?>
+
+            <div class="cadre_msg"
+            <div class="contentg">
+                <div class="msg_form">
+                    <form action="echg_msg.php?demandeur=<?php echo $demandeur;?>&proprietaire=<?php echo $proprietaire; ?>&logement=<?php echo $logement ;?>" method="post" xmlns="http://www.w3.org/1999/html">
+                        <label for="message">Message</label>
+                        <input type="text" name="message"> <br /><br />
+                        <input type="submit" value="Envoyer">
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</div>
+</body>
+</html>
