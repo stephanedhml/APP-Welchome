07c71465cc9e85ca669cdcab5ce4ff33202baa81
Geekelektro <beaudru.manuel@gmail.com>
2014-12-26 13:38:29 +0100

    Début de forum

---
+++ fichiers_php/forum.php
@@ -0,0 +1,16 @@
+<table>
+    <tr>
+        <th>Catégorie</th>
+        <th>Sujets</th>
+        <th>Messages</th>
+        <th>Dernier message</th>
+    </tr>
+    <tr>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+    </tr>
+</table>
+
+<!-- Ajouter table : categorie, topic, liste sujets, messages ?, -->
