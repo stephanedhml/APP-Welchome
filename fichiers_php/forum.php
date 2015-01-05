f7ccdd7ec342c16b8b562eb98d7f5c882aafaf10
Geekelektro <beaudru.manuel@gmail.com>
2015-01-05 14:56:46 +0100

    Début de forum

--- fichiers_php/forum.php
+++ fichiers_php/forum.php
@@ -1,16 +1,60 @@
-<table>
-    <tr>
-        <th>Catégorie</th>
-        <th>Sujets</th>
-        <th>Messages</th>
-        <th>Dernier message</th>
-    </tr>
-    <tr>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-    </tr>
-</table>
+<?php
+include("config.php");
+include("modeles.php");
+include("../menu_responsive/javascript/menu_responsive.js");
 
+session_start();
+?>
+
+<html>
+<head>
+    <meta charset="UTF-8">
+    <link rel="shortcut icon" href="../images_diverses/icon.png" type="image/x-icon"/>
+    <link rel="icon" href="../images_diverses/icon.png" type="image/x-icon"/>
+    <link rel="stylesheet" href="../style.css" />
+</head>
+<body>
+
+<header>
+    <?php include("menus.php"); ?>
+</header>
+<div class="superglobal">
+    <div class="global">
+
+        <table class="tableau_forum_accueil">
+            <tr>
+                <th>Catégorie</th>
+                <th>Description</th>
+                <th>Messages</th>
+                <th>Dernier message</th>
+            </tr>
+            <tr>
+                <td>Site</td>
+                <td>Toutes les news concernant le site (améliorations, propositions etc)</td>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <td>Boîte à idées</td>
+                <td>Propositions des utilisateurs pour améliorer le service proposé par l'équipe Welchome</td>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <td>Problèmes</td>
+                <td>Vous avez un problème ? On peut vous aider ! Venez exposer votre situation sur le forum !</td>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <td>Blabla Corner</td>
+                <td>Discussions en tous genres</td>
+                <td></td>
+                <td></td>
+            </tr>
+        </table>
+    </div>
+</div>
+</body>
+</html>
 <!-- Ajouter table : categorie, topic, liste sujets, messages ?, -->
--- fichiers_php/menus.php
+++ fichiers_php/menus.php
@@ -6,7 +6,7 @@
 		<ul class="clearfix">
 			<li><a href="accueilmanu.php" class="btn_ACCUEIL">Accueil</a></li>
 			<li class="recherche_avancee"><a href="recherche_avancee.php">Recherche avancée</a></li>
-			<li><a href="#" class="btn_FORUM">Forum</a></li>
+			<li><a href="forum.php" class="btn_FORUM">Forum</a></li>
             <?php
 				include("connexion_inscription_deconnexion_menu.php"); 
 			?>
