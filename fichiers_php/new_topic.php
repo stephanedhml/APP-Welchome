ab51b2e5a769731a0ebf22ffcdf4fc36f85a0e22
Geekelektro <beaudru.manuel@gmail.com>
2015-01-06 16:08:20 +0100

    Amélioration apparence publication d'un nouveau topic

--- fichiers_php/new_topic.php
+++ fichiers_php/new_topic.php
@@ -32,13 +32,13 @@ session_start();
                 echo '<div class="no_msg_r"><p><h7>Votre sujet à bien été posté, ainsi que le premier message</h7><br/><br/></p></div>';
             }
         else { echo '
-                        <div class="cadrec">
-                                <div class="connex1">
+                        <div class="cadre_topic_post">
+                                <div class="answer1">
                                     <form action="new_topic.php?id_topic='. $_GET['id_topic'] .'" method="post">
-                                        <label for="username" id="username_form">Objet</label><br/>
-                                        <input type="text" name="objet" id="objet" /><br /><br/>
-                                        <label for="password">Message</label><br/>
-                                        <input type="text" name="message" id="message" /><br /><br/>
+                                        <label for="username" id="username_form">Objet</label><br/></br>
+                                        <input type="text" name="objet" class="objet_field" /><br /><br/>
+                                        <label for="password">Message</label><br/></br>
+                                        <input type="text" name="message" class="message_field" /><br /><br/></br>
                                         <input type="submit" value="Publier" id="btn_connexion" /><br/><br/>
                                     </form>
                                 </div>
