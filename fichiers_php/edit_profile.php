67ca42951dfa217ef5b4c7e8d3af9b69173efb72
Manuel <beaudru.manuel@gmail.com>
2015-01-10 01:08:04 +0100

    Ajout d'un nombre considérable de critères éditables via formulaire ainsi que de la BDD la plus complète actuellement

--- fichiers_php/edit_profile.php
+++ fichiers_php/edit_profile.php
@@ -62,6 +62,87 @@ if (isset($_POST["situation"])) {
         'id_user' => $_SESSION['userid'],
     ));
 }
+
+// Logement
+
+if (isset($_POST["localisation"])) {
+    $desc = $bdd -> prepare("UPDATE logement SET localisation=:localisation WHERE id_users=:id_user");
+    $desc -> execute(array(
+        'localisation' => $_POST['localisation'],
+        'id_user' => $_SESSION['userid'],
+    ));
+}
+
+if (isset($_POST["description_logement"])) {
+    $desc = $bdd -> prepare("UPDATE logement SET description_logement=:description WHERE id_users=:id_user");
+    $desc -> execute(array(
+        'description' => $_POST['description_logement'],
+        'id_user' => $_SESSION['userid'],
+    ));
+}
+if (isset($_POST["type_logement"])) {
+    $desc = $bdd -> prepare("UPDATE logement SET type_logement=:type_logement WHERE id_users=:id_user");
+    $desc -> execute(array(
+        'type_logement' => $_POST['type_logement'],
+        'id_user' => $_SESSION['userid'],
+    ));
+}
+
+if (isset($_POST["nom_maison"])) {
+    $desc = $bdd -> prepare("UPDATE logement SET nom_maison=:nom_maison WHERE id_users=:id_user");
+    $desc -> execute(array(
+        'nom_maison' => $_POST['nom_maison'],
+        'id_user' => $_SESSION['userid'],
+    ));
+}
+
+if (isset($_POST["nombre_voyageurs"])) {
+    $desc = $bdd -> prepare("UPDATE logement SET nombre_voyageurs=:nombre_voyageurs WHERE id_users=:id_user");
+    $desc -> execute(array(
+        'nombre_voyageurs' => $_POST['nombre_voyageurs'],
+        'id_user' => $_SESSION['userid'],
+    ));
+}
+
+if (isset($_POST["nombre_chambres"])) {
+    $desc = $bdd -> prepare("UPDATE logement SET nb_chambres=:nombre_chambres WHERE id_users=:id_user");
+    $desc -> execute(array(
+        'nombre_chambres' => $_POST['nombre_chambres'],
+        'id_user' => $_SESSION['userid'],
+    ));
+}
+
+if (isset($_POST["nb_salles_bains"])) {
+    $desc = $bdd -> prepare("UPDATE logement SET nb_salles_bains=:nb_salles_bains WHERE id_users=:id_user");
+    $desc -> execute(array(
+        'nb_salles_bains' => $_POST['nb_salles_bains'],
+        'id_user' => $_SESSION['userid'],
+    ));
+}
+
+if (isset($_POST["superficie"])) {
+    $desc = $bdd -> prepare("UPDATE logement SET superficie=:superficie WHERE id_users=:id_user");
+    $desc -> execute(array(
+        'superficie' => $_POST['superficie'],
+        'id_user' => $_SESSION['userid'],
+    ));
+}
+
+if (isset($_POST["date_début_disponibilite"])) {
+    $desc = $bdd -> prepare("UPDATE logement SET date_début_disponibilite=:date_début_disponibilite WHERE id_users=:id_user");
+    $desc -> execute(array(
+        'date_début_disponibilite' => $_POST['date_début_disponibilite'],
+        'id_user' => $_SESSION['userid'],
+    ));
+}
+
+if (isset($_POST["date_fin_disponibilite"])) {
+    $desc = $bdd -> prepare("UPDATE logement SET date_fin_disponibilite=:date_fin_disponibilite WHERE id_users=:id_user");
+    $desc -> execute(array(
+        'date_fin_disponibilite' => $_POST['date_fin_disponibilite'],
+        'id_user' => $_SESSION['userid'],
+    ));
+}
 ?>
 
 
@@ -102,6 +183,38 @@ if (isset($_POST["situation"])) {
             <OPTION>Couple
             <OPTION>Marié
         </SELECT></br>
+
+        <!-- Logement -->
+        <label for="localisation">Localisation</label><br/>
+        <input type="text" name="localisation"/><br/>
+        <label for="description_logement">Description du logement</label><br/>
+        <input type="text" name="description_logement"/><br/>
+        <label for="type_logement">Type de logement</label><br/>
+        <select name="type_logement" id="choix">
+            <option value="Studio">studio</option>
+            <option value="Appartement">appartement</option>
+            <option value="Maison">maison</option>
+            <option value="Pavillon">pavillon</option>
+            <option value="Bungalow/gite">bungalow</option>
+            <option value="Bateau/péniche">bateau</option>
+            <option value="Camping car">caming car</option>
+        </select><br/>
+        <label for="nom_maison">Nom maison</label><br/>
+        <input type="text" name="nom_maison"/><br/>
+        <label for="nombre_voyageurs">Nombre de voyageurs permis</label><br/>
+        <input type="number" name="nombre_voyageurs"/><br/>
+        <label for="nombre_chambres">Nombre de chambres</label><br/>
+        <input type="number" name="nombre_chambres"/><br/>
+        <label for="nb_salles_bains">Nombre de salles de bain</label><br/>
+        <input type="number" name="nb_salles_bains"/><br/>
+        <label for="superficie">Superficie (en m2)</label><br/>
+        <input type="number" name="superficie"/><br/>
+        <!-- CHAMPS POUR DATES DISPO LOGEMENT
+        <label for="date_début_disponibilite">Date de début de disponibilité</label><br/>
+        <input type="date" name="date_début_disponibilite"/><br/>
+        <label for="date_fin_disponibilite">Date de fin de disponibilité</label><br/>
+        <input type="date" name="date_fin_disponibilite"/><br/> -->
+
         <input type="submit" value="Valider les modifications" id="btn_connexion" /><br/><br/>
     </form>
 </body>
