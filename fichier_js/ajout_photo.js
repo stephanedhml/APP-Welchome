5a946d3088a6335d1711f37fac77914a590641cf
Oclean14 <marvinroche14@gmail.com>
2015-01-13 16:15:23 +0100

    fichier ajout photo

---
+++ fichier_js/ajout_photo.js
@@ -0,0 +1,41 @@
+/**
+ * Created by marvin on 13/01/15.
+ */
+
+function verifphoto2(c)
+{
+    if(c.value == null)
+    {
+        alert('Uploadez d\'abord la photo 1');
+        return false;
+    }
+    else
+    {
+
+        return true;
+    }
+}function verifphoto3(c)
+{
+    if(c.value == null)
+    {
+        alert('Uploadez d\'abord la photo 2');
+        return false;
+    }
+    else
+    {
+
+        return true;
+    }
+}function verifphoto4(c)
+{
+    if(c.value == null)
+    {
+        alert('Uploadez d\'abord la photo 3');
+        return false;
+    }
+    else
+    {
+
+        return true;
+    }
+}
