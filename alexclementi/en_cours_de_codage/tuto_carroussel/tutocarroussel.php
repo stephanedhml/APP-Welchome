386f594f78c210ab2fbc3fb3141f2ebf5696ded7
Manuel <beaudru.manuel@gmail.com>
2015-01-18 14:01:32 +0100

    Suppression des fichiers inutiles (conservés dans un dossier sur mon ordi cependant, on sait jamais)

--- alexclementi/en_cours_de_codage/tuto_carroussel/tutocarroussel.php
+++
@@ -1,7 +0,0 @@
-<ul class="carrousel">
-	<li>A</li>
-	<li>B</li>
-	<li>C</li>
-	<li>D</li>
-	<li>E</li>
-</ul>
--- fichiers_php/config.php
+++ fichiers_php/config.php
@@ -1,9 +1,9 @@
-<!-- Module de connexion à la base de donnée avec vérification d'erreur -->
+﻿<!-- Module de connexion à la base de donnée avec vérification d'erreur -->
 
 <?php
 try
 	{
-		$bdd = new PDO ('mysql:host=localhost;dbname=welchome', 'root', 'root',array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+		$bdd = new PDO ('mysql:host=localhost;dbname=welchome', 'root', '',array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
 }
 catch(Exception $e)
 	{
